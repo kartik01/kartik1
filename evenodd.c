cebffef1946a3290d1c02182c0db301cd839fc30
kartik01 <53349544+kartik01@users.noreply.github.com>
2020-03-29 17:45:54 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include<conio.h> 
+ void main()  
+{  
+    int n;    
+printf("enter a number:");    
+scanf("%d",&n);     
+if(n%2==0)
+{    
+printf("the number is even");    
+}    
+else
+{    
+printf("the number is odd");    
+}     
+    getch();
+    }
+© 2020 GitHub, Inc.
